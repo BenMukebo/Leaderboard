e226dcb589fa0c2d887be78821b883d616a07513
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-06 08:38:43 +0200

    Update config files

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
+    index: './src/js/index.js',
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -21,6 +21,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
