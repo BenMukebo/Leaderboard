4f546ce720bcb3bef3bd5cb24dfd969122ab1d78
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-06 08:43:56 +0200

    Import the renderData function to the index.js file

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '../css/style.css';
+import { renderData, data } from './data';
+
+const form = document.querySelector('form');
+const inputName = document.querySelector('.inputName input');
+const inputScore = document.querySelector('.inputScore input');
+
+renderData(data);
+
+
