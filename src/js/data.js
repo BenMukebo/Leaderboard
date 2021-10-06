bbe3cf32e461a470b28faefd43addc0682f2d791
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-06 08:41:15 +0200

    Create a data.js file and store all player their information in an data array

---
+++ src/js/data.js
@@ -0,0 +1,10 @@
+export const data = [
+  { player: 'name', score: 100 },
+  { player: 'name', score: 20 },
+  { player: 'name', score: 50 },
+  { player: 'name', score: 125 },
+  { player: 'name', score: 77 },
+];
+  
+const container = document.querySelector('.score-list');
+
