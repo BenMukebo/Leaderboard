27ee474282f609d72b6b22a2baa621b504614ac3
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-07 15:37:56 +0200

    Add a postResponse function to post the newPlayer object

--- src/js/api.js
+++ src/js/api.js
@@ -1,3 +1,15 @@
 import renderPlayer from './data.js';
 
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RXGEUgHFa9saDlLAcSKm/scores';
+
+export const postResponse = async (newPlayer) => {
+  const post = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ user: newPlayer.user, score: newPlayer.score }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const response = await post.json();
+  return response;
+};
