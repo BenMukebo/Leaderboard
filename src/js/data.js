0bb09ffe31fb9932a149d1bbf2cc85a49a7058fe
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-08 08:11:55 +0200

    Add some color the project

--- src/js/data.js
+++ src/js/data.js
@@ -5,7 +5,8 @@ const renderPlayer = (data) => {
   data.forEach((game) => {
     const li = document.createElement('li');
     li.classList.add('score-list-item');
-    li.innerHTML = `${game.user}: <span> ${game.score}</span>`;
+    li.innerHTML = `<div><img src="../img/games.jpg" alt=""> ${game.user} : </div>
+    <span> ${game.score}</span>`;
     container.appendChild(li);
   });
 };
