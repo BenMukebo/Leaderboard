7e24adb6d10c8885a083d75ce3f61d7140b0c515
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-08 09:49:39 +0200

    Add image to the input

--- src/js/data.js
+++ src/js/data.js
@@ -1,11 +1,16 @@
+import image1 from '../img/games.jpg';
+import image2 from '../img/game.jpeg';
+
+const images = [image1, image2, image1, image2, image1, image2, image1, image2];
+
 const container = document.querySelector('.score-list');
 
 const renderPlayer = (data) => {
   container.innerHTML = '';
-  data.forEach((game) => {
+  data.forEach((game, index) => {
     const li = document.createElement('li');
     li.classList.add('score-list-item');
-    li.innerHTML = `<div><img src="../img/games.jpg" alt=""> ${game.user} : </div>
+    li.innerHTML = `<div class="person"><img src=${images[index]} alt=""> ${game.user} : </div>
     <span> ${game.score}</span>`;
     container.appendChild(li);
   });
