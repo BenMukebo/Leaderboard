6bea3a9d4e3305a7d66dce2425ef18c2b98cc942
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-04-20 23:45:45 +0200

    Add throw new Error message

--- src/js/api.js
+++ src/js/api.js
@@ -3,20 +3,33 @@ import renderPlayer from './data.js';
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xjollLOeoeW5XMm3S911/scores';
 
 export const postResponse = async (newPlayer) => {
-  const post = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({ user: newPlayer.user, score: newPlayer.score }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  const response = await post.json();
-  return response;
+  try {
+    const post = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({ user: newPlayer.user, score: newPlayer.score }),
+    });
+    const response = await post.json();
+    return response;
+  } catch (error) {
+    // throw new Error(error.message);
+    throw new Error('can not create gam');
+  }
 };
 
 export const getResponse = async () => {
   const get = await fetch(url);
-  const response = await get.json();
-  const sortedGameData = response.result.sort((a, b) => (b.score > a.score ? 1 : -1));
-  renderPlayer(sortedGameData);
+  try {
+    const response = await get.json();
+    const sortedGameData = response.result.sort((a, b) => (b.score > a.score ? 1 : -1));
+    renderPlayer(sortedGameData);
+  } catch (err) {
+    console.log(err.message);
+  }
 };
+
+// if(response.status !== 200){
+//   throw new Error('can not fetch the data');
+// }
