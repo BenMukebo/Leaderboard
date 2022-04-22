0c5fe2feed62303afe121fa4b31f11147bc5963b
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-04-22 15:36:01 +0200

    Generate a random image

--- src/js/data.js
+++ src/js/data.js
@@ -1,18 +1,20 @@
 import image1 from '../img/game1.jpg';
 import image2 from '../img/game2.jpg';
-// import image2 from '../img/game3.jpeg';
+import image3 from '../img/game3.jpeg';
+import image4 from '../img/game4.png';
+import image5 from '../img/game5.jpeg';
 
-// eslint-disable-next-line max-len
-const images = [image1, image2, image1, image2, image1, image2, image1, image2, image1, image2, image1, image2];
-
+const images = [image1, image2, image3, image4, image5];
 const container = document.querySelector('.score-list');
 
 const renderPlayer = (data) => {
   container.innerHTML = '';
-  data.forEach((game, index) => {
+  data.forEach((game) => {
+    const image = images[Math.floor(Math.random() * images.length)];
+
     const li = document.createElement('li');
     li.classList.add('score-list-item');
-    li.innerHTML = `<div class="person"><img src=${images[index]} alt=""> ${game.user} : </div>
+    li.innerHTML = `<div class="person"><img src=${image} alt=""> ${game.user} : </div>
     <span> ${game.score}</span>`;
     container.appendChild(li);
   });
