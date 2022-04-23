42b15b3101f72aad60a7961a9280292a5dcd1954
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-04-23 23:44:05 +0200

    Fix js linter errors

--- src/js/api.js
+++ src/js/api.js
@@ -4,23 +4,19 @@ const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/game
 // const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RXGEUgHFa9saDlLAcSKm/scores';
 
 export const postResponse = async (newPlayer) => {
-  try {
-    const post = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-      body: JSON.stringify({ user: newPlayer.user, score: newPlayer.score }),
-    });
+  const post = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({ user: newPlayer.user, score: newPlayer.score }),
+  });
 
-    if (post.status !== 200) {
-      throw new Error('You can POST your data here.');
-    }
-    const response = await post.json();
-    return response;
-  } catch (error) {
-    console.log(error.message);
+  if (post.status !== 200) {
+    throw new Error('You can POST your data here.');
   }
+  const response = await post.json();
+  return response;
 };
 
 export const getResponse = async () => {
@@ -33,6 +29,6 @@ export const getResponse = async () => {
     const sortedGameData = response.result.sort((a, b) => (b.score > a.score ? 1 : -1));
     renderPlayer(sortedGameData);
   } catch (err) {
-    console.log(err.message);
+    // console.log(err.message);
   }
 };
